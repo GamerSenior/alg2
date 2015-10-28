39d00c7f6cddd4191b3a76125700371642f8ddb8
Giovani Garcia <abelgiovani@gmail.com>
2015-10-28 20:33:55 -0200

    Adicionado menu de ajuda - Bug ao sair dele, printw bugado

--- database_linux.c
+++ database_linux.c
@@ -11,52 +11,61 @@
 
 int main(){
 	short c, opc, x, y;
-	x = 0; y = 1; opc = 1;
 
 	setlocale(LC_ALL, "");
 	initscr();
 	raw();
 	noecho();
 	keypad(stdscr, TRUE);
-	printw("-----DATABASE EVIL CORP-----.\n");
-	printw("1. Cadastro\n2. Whatever\n3. Whatever\n4. Whatever\n5. Whatever\n6. Whatever");
-	refresh();
-
+	
 	do{
-		move(y,x);
-		c = getch();
+		system("clear");
+		mvprintw(0,0,"-----DATABASE EVIL CORP-----\n");
+		printw("1. Cadastro\n2. Whatever\n3. Whatever\n4. Whatever\n5. Whatever\n6. Whatever");
+		x = 0; y = 1; opc = 1;
+		refresh();
+		do{
+			move(y,x);
+			c = getch();
 
-		if(c == 'Q' || c == 'q')
-			opc=0;
+			if(c == 'Q' || c == 'q'){
+				opc=0;
+				c=0;
+			}
 
-		if(c == KEY_F(1)){
-			printw("MENU DE AJUDA");
-		}
+			if(c == KEY_F(2)){
+				system("clear");
+				mvprintw(0, 0, "MENU DE AJUDA");
+				x = 0, y = 1, opc = 0;
+				refresh();
+				getch();
+			}
 
-		switch(c){
-			case KEY_UP:
-				if(opc>1){
-					opc--;
-					y--;
-				}
+			switch(c){
+				case KEY_UP:
+					if(opc>1){
+						opc--;
+						y--;
+					}
 
-				else{
-					opc=MAX;
-					y=MAX;
-				}
-			break;
+					else{
+						opc=MAX;
+						y=MAX;
+					}
+				break;
 
-			case KEY_DOWN:
-				if(opc<6){
-					opc++;
-					y++;
-				}
-				else{
-					opc=MIN;
-					y=MIN;
-				}
-		}
-	}while(opc);
+				case KEY_DOWN:
+					if(opc<6){
+						opc++;
+						y++;
+					}
+					else{
+						opc=MIN;
+						y=MIN;
+					}
+			}
+		}while(opc);
+	}while(c);
 
 	endwin();
 	return 0;
